4123026cf35c3581d36befbd24974e2deb4d7759
Dan Marino <technicaldanny302@gmail.com>
2024-10-03 18:55:02 -0700

    completed exercise (2)

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -13,8 +13,12 @@ namespace Testing.Controllers
         public IActionResult Index()
         {
             var products = repo.GetAllProducts();
-            return View(products);             
-            
+            return View(products);
+        }
+        public IActionResult ViewProduct(int id)
+        {
+            var product = repo.GetProduct(id);
+            return View(product);
         }
     }
 }
